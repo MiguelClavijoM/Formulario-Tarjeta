03cda6abf70a9e2eef9020f40fe39710cffd5fa3
Miguel <miguelclavijo03@gmail.com>
2022-09-18 01:46:11 -0500

    Interfaz de datos financieros

--- src/app/datos-financieros/datos-financieros.component.ts
+++ src/app/datos-financieros/datos-financieros.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-datos-financieros',
   templateUrl: './datos-financieros.component.html',
   styleUrls: ['./datos-financieros.component.scss']
 })
-export class DatosFinancierosComponent implements OnInit {
 
-  constructor() { }
-
+export class DatosFinancierosComponent implements OnInit{
   ngOnInit(): void {
   }
 
+  formulario2: FormGroup;
+
+  tiposDocumentos2 = ['Doctor/a', 'Ingeniero/a', 'Maestro/a', 'Otro' ]
+  tiposDocumentos3 = ['Contrato fijo', 'Contrato indefinido', 'Por prestación de servicios', 'Por obra o labor']
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.formulario2 = this.formBuilder.group({
+      ingresosMensuales: [null, [Validators.required]],
+      gastosMensuales: [null,[Validators.required]],
+      ocupacion: [null],
+      tipoContrato: [null,[Validators.required]]
+    });
+}
+
+submit(){
+  this.router.navigate(['resultados'])
+}
 }
